75727eb8553f6d0407da04432c4c70eb67a3a74f
EisakuHiguchi <deosohyaish@gmail.com>
2018-09-30 23:55:01 +0900

    refactor code

--- scholar/network_from_json.py
+++ scholar/network_from_json.py
@@ -6,27 +6,19 @@ import collections
 import itertools
 import networkx as nx
 import matplotlib.pyplot as plt
-
 from datetime import datetime
-nowtime = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+import argparse
 
-FILENAME_JSON = input("Filename:")
-#FILENAME_JSON = os.path.basename(FILENAME_JSON)
+import format_json
 
-#FILENAME_JSON = "SEARCH_BOT_for_scholar_groebner computer graphics.json"
-FILENAME_SAVEFIG = nowtime + "_" + os.path.basename(FILENAME_JSON).split("SEARCH_BOT_for_scholar_")[1].split(".")[0]
-FILENAME_SAVEFIG_CLOUD = FILENAME_SAVEFIG + "_CLOUD"
-FILENAME_SAVEFIG_CLOUD2 = FILENAME_SAVEFIG + "_WORDS_CLOUD"
 
-def loadjson():
-  f = open(FILENAME_JSON, "r", encoding="utf-8")
-  jsonData = json.load(f)
-  f.close()
-  return jsonData
+def get_filename_savefig(filename_json):
+  nowtime = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+  return nowtime + "_" + os.path.basename(filename_json).split(".")[0]
 
-def all_tags_list():
+def all_tags_list(filename_json):
   taglist = []
-  jsonData = loadjson()
+  jsonData = format_json.loadjson(filename_json)
   for key in jsonData.keys():
     taglist.append(jsonData[key]["tags"])
   tag_count = collections.Counter(itertools.chain.from_iterable(taglist))
@@ -37,20 +29,7 @@ def all_tags_list():
     result.append(e[0]) 
   return result
 
-
-def create_tag_cloud():
-  jsonData = loadjson()
-  taglist = [] 
-  for key in jsonData.keys():
-    taglist.append(jsonData[key]["tags"])
-  tag_count = collections.Counter(itertools.chain.from_iterable(taglist))
-  deltemp = tag_count.most_common(1)
-  tag_count.pop(deltemp[0][0])
-
-
-  G = nx.Graph()
-  G.add_nodes_from([(tag, {"count":count}) for tag, count in tag_count.most_common()])
-              
+def add_edges(G, taglist):
   for tags in taglist:
     for tag0, tag1 in itertools.combinations(tags, 2):
       if not G.has_node(tag0) or not G.has_node(tag1):
@@ -59,22 +38,46 @@ def create_tag_cloud():
         G[tag0][tag1]["weight"] += 1
       else:
         G.add_edge(tag0, tag1, weight=1)
+  return G
 
-  plt.figure(figsize=(15,15)) # グラフのサイズを定義
-  pos = nx.spring_layout(G, k=1.5) # ノード間の反発力を定義。値が小さいほど密集する
+def define_plt_pram(G):
+  plt.figure(figsize=(15,15))
+  pos = nx.spring_layout(G, k=1.5)
 
-  node_size = [ d['count']*50 for (n,d) in G.nodes(data=True)] # ノードの大きさを調整
-  nx.draw_networkx_nodes(G, pos, node_color='b', alpha=0.2, node_size=node_size, font_weight="bold", font_family='VL Gothic') # ノードのスタイルを定義
+  node_size = [ d['count']*50 for (n,d) in G.nodes(data=True)]
+  nx.draw_networkx_nodes(G, pos, node_color='b', alpha=0.2, node_size=node_size, font_weight="bold", font_family='VL Gothic')
   nx.draw_networkx_labels(G, pos, fontsize=14)
 
-  edge_width = [ d['weight']*0.5 for (u,v,d) in G.edges(data=True)] # エッジの太さを調整
-  nx.draw_networkx_edges(G, pos, alpha=0.4, edge_color='r', width=edge_width) # エッジのスタイルを定義
+  edge_width = [ d['weight']*0.5 for (u,v,d) in G.edges(data=True)]
+  nx.draw_networkx_edges(G, pos, alpha=0.4, edge_color='r', width=edge_width)
 
+
+def plot_save_nx(G, filename_plot):
   plt.axis('off')
-  plt.savefig(FILENAME_SAVEFIG_CLOUD)
+  plt.savefig(filename_plot)
   #plt.show()
   plt.close()
   G.clear()
+
+def create_tag_cloud(prm, filename_json):
+  jsonData = format_json.loadjson(filename_json)
+  taglist = [] 
+  for key in jsonData.keys():
+    taglist.append(jsonData[key]["tags"])
+  tag_count = collections.Counter(itertools.chain.from_iterable(taglist))
+  deltemp = tag_count.most_common(1)
+  tag_count.pop(deltemp[0][0])
+
+  G = nx.Graph()
+  if prm == 0:
+    G.add_nodes_from([(tag, {"count":count}) for tag, count in tag_count.most_common()])
+  else:
+    G.add_nodes_from([(tag, {"count":count}) for tag, count in tag_count.most_common(prm)])
+  G = add_edges(G, taglist)
+  define_plt_pram(G)
+
+  filename_plot = get_filename_savefig(filename_json) + "_CLOUD"
+  plot_save_nx(G, filename_plot)
 
 def remove_lesswords(listdata):
   temp = []
@@ -83,11 +86,8 @@ def remove_lesswords(listdata):
         temp.append(e.lower().replace(",","").replace(".",""))
   return temp
 
-def create_text_cloud(prm):
-  prm_mostcommon = prm # prm_mostcommon = 20
-  prm_least_flag = False
-
-  jsonData = loadjson()
+def create_text_cloud(prm, filename_json, prm_least_flag = False):
+  jsonData = format_json.loadjson(filename_json)
   taglist = [] 
   
   for key in jsonData.keys():
@@ -100,38 +100,19 @@ def create_text_cloud(prm):
   listlen = len(tag_count.most_common())
   
   if prm_least_flag:
-    G.add_nodes_from([(tag, {"count":count}) for tag, count in tag_count.most_common()[-prm_mostcommon:]])
+    G.add_nodes_from([(tag, {"count":count}) for tag, count in tag_count.most_common()[-prm:]])
   else:
-    G.add_nodes_from([(tag, {"count":count}) for tag, count in tag_count.most_common(prm_mostcommon)])
-              
-  for tags in taglist:
-    for tag0, tag1 in itertools.combinations(tags, 2):
-      if not G.has_node(tag0) or not G.has_node(tag1):
-        continue
-      if G.has_edge(tag0, tag1):
-        G[tag0][tag1]["weight"] += 1
-      else:
-        G.add_edge(tag0, tag1, weight=1)
+    G.add_nodes_from([(tag, {"count":count}) for tag, count in tag_count.most_common(prm)])
+  
+  G = add_edges(G, taglist)
+  define_plt_pram(G)
 
-  plt.figure(figsize=(15,15)) # グラフのサイズを定義
-  pos = nx.spring_layout(G, k=1.5) # ノード間の反発力を定義。値が小さいほど密集する
-
-  node_size = [ d['count']*50 for (n,d) in G.nodes(data=True)] # ノードの大きさを調整
-  nx.draw_networkx_nodes(G, pos, node_color='b', alpha=0.2, node_size=node_size, font_weight="bold", font_family='VL Gothic') # ノードのスタイルを定義
-  nx.draw_networkx_labels(G, pos, fontsize=14)
-
-  edge_width = [ d['weight']*0.5 for (u,v,d) in G.edges(data=True)] # エッジの太さを調整
-  nx.draw_networkx_edges(G, pos, alpha=0.4, edge_color='r', width=edge_width) # エッジのスタイルを定義
-
-  plt.axis('off')
-  plt.savefig(FILENAME_SAVEFIG_CLOUD2)
-  #plt.show()
-  plt.close()
-  G.clear()
+  filename_plot = get_filename_savefig(filename_json) + "_WORDS_CLOUD"
+  plot_save_nx(G, filename_plot)
 
 
-def create_year_map():
-  jsonData = loadjson()
+def create_year_map(filename_json):
+  jsonData = format_json.loadjson(filename_json)
   taglist = []
   for key in jsonData.keys():
     temp = jsonData[key]["tags"]
@@ -140,7 +121,6 @@ def create_year_map():
       temp.append(int(year))
       taglist.append(temp)
     
-  max_year = 0
   min_year = 3000
   dict_taglist = {}
   for e in taglist:
@@ -152,9 +132,6 @@ def create_year_map():
     else:
       temp = set(e[1:-1])
       dict_taglist[e[-1]] = temp
-    
-    if max_year < e[-1]:
-      max_year = e[-1]    
     if min_year > e[-1]:
       min_year = e[-1]
 
@@ -163,7 +140,7 @@ def create_year_map():
 
   G = nx.Graph()
 
-  all_tags = all_tags_list()
+  all_tags = all_tags_list(filename_json)
   temp = []
   for e in dict_taglist.keys():
     tags = dict_taglist[e]
@@ -188,36 +165,37 @@ def create_year_map():
     for s in same_tags:
       G.add_edge(str(keys[i-1])+"_"+s, str(keys[i])+"_"+s, weight=1)
     b_tags = a_tags
-
-  print("debug")
+  
   nx.draw_networkx_edges(G, pos, alpha=0.4, edge_color='r', width=2)
-  plt.axis('off')
-  plt.savefig(FILENAME_SAVEFIG)
-  #plt.show()
-  plt.close()
+  filename_plot = get_filename_savefig(filename_json)
+  plot_save_nx(G, filename_plot)
+  
 
+# ---- ----
+# main
 
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('Filename', metavar='F', type=str, nargs='+',help='Filename for saving data')
+  parser.add_argument("-w", "--words", type=bool, default=False)
+  parser.add_argument("-wp", "--wordprm", type=int, default=15)
+  parser.add_argument("-t", "--tags", type=bool, default=True)
+  parser.add_argument("-tp", "--tagprm", type=int, default=0)
+  parser.add_argument("-y", "--years", type=bool, default=False)
+  args = parser.parse_args()
 
-option = input("option:")
+  filename_json = args.Filename[0]
 
-# words, tag, year
-Flags = [False, False, False]
-
-if option == "a":
-  Flags = [True, True, True]
-if option == "w":
-  Flags[0] = True
-elif option == "t":
-  Flags[1] = True
-elif option == "y":
-  Flags[2] = True
-
-if Flags[0]:
-  print("create words_cloud")
-  create_text_cloud(15)
-if Flags[1]:
-  print("create tag_cloud")
-  create_tag_cloud()
-if Flags[2]:
-  print("create year_map")
-  create_year_map()
+  if args.words:
+    print("create words_cloud:" + str(args.wordprm))
+    create_text_cloud(args.wordprm, filename_json)
+  if args.tags:
+    if args.tagprm == 0:
+      print("create tag_cloud: all")
+    else:
+      print("create tag_cloud:" + str(args.tagprm))
+    
+    create_tag_cloud(args.tagprm, filename_json)
+  if args.years:
+    print("create year_map")
+    create_year_map(filename_json)
