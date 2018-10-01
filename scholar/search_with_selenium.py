1c47dd81017cae4c41f25d32448a77a8549edcfe
EisakuHiguchi <deosohyaish@gmail.com>
2018-10-01 14:26:03 +0900

    minor change

--- scholar/search_with_selenium.py
+++ scholar/search_with_selenium.py
@@ -14,7 +14,7 @@ import format_json
 print("Now loading Selenium. Please wait while standing up Webbrowser.")
 driver = webdriver.Edge("MicrosoftWebDriver.exe")
 driver.get("https://scholar.google.co.jp/")
-driver.set_window_size(1000, 1000)
+#driver.set_window_size(500, 500)
 
 def start_search(KEYWORD):
   driver.find_element_by_id("gs_hdr_tsi").send_keys(KEYWORD)
@@ -61,23 +61,13 @@ def get_dict_link_data(list_dict_result):
   
   return list_dict_result
 
-
-def save(str_result):
-  print("saving data...")
-  f = open("SEARCH_BOT_for_scholar_" + SEARCH_KEY + ".md", "w", encoding="utf-8")
-  f.write(str_result)
-  f.close()
-  print("save done!")
-
-def savejson_dict(list_dict_results):
+def savejson(list_dict_results, filename):
   count = 1
   json_data_temp = {}
   for e in list_dict_results:
     json_data_temp["id_" + str(count)] = e
     count += 1
-  
-  filenmae_json = "SEARCH_BOT_for_scholar_" + SEARCH_KEY.replace(" ", "_") + ".json"
-  format_json.savejson_dict(json_data_temp, filenmae_json)
+  format_json.savejson_dict(json_data_temp, filename)
   
   
 # ---- ---- ---- ----
@@ -90,66 +80,29 @@ args = argparser.parse_args()
 DEBUG = args.debug
 list_dict_results = []
 
-
-command = input("SEARCH_KEY:")
-
-if command == "continue":
-  filename = input("FILEPATH:")
-  dict_json = format_json.loadjson(filename)
-  if dict_json == -1:
-    driver.close()
-    sys.exit()
+filename = input("FILEPATH:")
+dict_json = format_json.loadjson(filename)
+if dict_json == -1:
+  print("error: Not Found file. create new file " + filename)
+  #driver.close()
+  #sys.exit()
+else:
   for key in dict_json.keys():
     list_dict_results.append(dict_json[key])
-  command = input("SEACH_KEY:")
-  #start_search(command)
-  SEARCH_KEY = command
-else:
-  #str_result = "# " + command + " :: SEARCH_BOT for scholar\n"
-  #str_result += "----\n\n"
-  #str_result = get_link_data(str_result)
-  start_search(command)
-  SEARCH_KEY = command
-  print("getting first page data...")
-  list_dict_results = get_dict_link_data(list_dict_results)
-
-
-
-count = 0
-count_end = 0
 
 if DEBUG:
   print("debug mode")
 else:
   while command != "end":
-    if count == 0:
-      command = input(">>> ")
-    else:
-      click_next()
-      list_dict_results = get_dict_link_data(list_dict_results)
-      sleeping = random.uniform(0.5,3)
-      print("count:" + str(count) + " sleep:" + str(sleeping))
-      sleep(sleeping)
-      count += 1
+    command = input(">>> ")
 
     if command == "next":
       click_next()
-      #str_result = get_link_data(str_result)
       list_dict_results = get_dict_link_data(list_dict_results)
     if command == "reload":
-      #str_result = get_link_data(str_result)
       list_dict_results = get_dict_link_data(list_dict_results)
-    if "count" in command:
-      count = 1
-      count_end = int(input("count(int)= ")) + 1
-      command = "none"
 
-
-    if count == count_end:
-      count = 0
-
-  #save(str_result)
-  savejson_dict(list_dict_results)
+  savejson(list_dict_results, filename)
   driver.close()
   print("done!")
 
